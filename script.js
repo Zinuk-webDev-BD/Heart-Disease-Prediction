237f1ba9ab93f48a7450b3abed3dd761764153cd
Kazi Nigar Sultana Zinuk <zinuk.web.dev.bd@gmail.com>
2026-05-23 14:53:13 +0600

    updates

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-document.getElementById("heartForm").addEventListener("submit", async function(e) {
+const form = document.getElementById("heartForm");
+const resultBox = document.getElementById("result");
+
+form.addEventListener("submit", async function(e) {
+
   e.preventDefault();
+
+  resultBox.innerHTML = "Checking Prediction...";
 
   const data = {
     age: +document.getElementById("age").value,
@@ -12,13 +18,50 @@ document.getElementById("heartForm").addEventListener("submit", async function(e
     exang: +document.getElementById("exang").value
   };
 
-  const res = await fetch("http://127.0.0.1:5000/predict", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  });
+  try {
 
-  const result = await res.json();
+    const res = await fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
 
-  document.getElementById("result").innerText = result.prediction;
+    const result = await res.json();
+
+    resultBox.innerHTML = result.prediction;
+
+    if(result.prediction.toLowerCase().includes("risk")) {
+      resultBox.style.color = "#ff4b5c";
+    }
+    else {
+      resultBox.style.color = "#4ade80";
+    }
+
+  }
+
+  catch(error) {
+
+    resultBox.innerHTML = "Server Error!";
+    resultBox.style.color = "orange";
+
+  }
+
+});
+
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("light-mode");
+
+  if(document.body.classList.contains("light-mode")) {
+    themeBtn.innerHTML = `<i class="fa-solid fa-sun"></i>`;
+  }
+  else {
+    themeBtn.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+  }
+
 });
